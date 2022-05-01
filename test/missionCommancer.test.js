4ba6ead0bbc06f07a648b25f3e39e6dfbfb4b767
pe-ca <mr.gloser1@gmail.com>
2022-05-01 13:36:12 -0500

    modificacion de test

--- test/missionCommancer.test.js
+++ test/missionCommancer.test.js
@@ -1,10 +1,8 @@
-describe(
-    "Esto es una suite de pruebas", () =>{
-        test(
-            'Caso de prueba 1', ()=>{
-                const result = 1+2
-                expect(result).toBe(3)
-            }
-        )
-    }
-)
+const MissionCommander = require('./../app/missionCommander')
+
+describe("UnitTest for Mission Commander class", () =>{
+    test('Caso de prueba 1', ()=>{
+        const missionCommander = new MissionCommander("Woopa")
+        expect(missionCommander.name).toBe("Woopa")
+    })
+})
