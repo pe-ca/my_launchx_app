32d0aa1d490a560b5e90ad849680eed527f6592c
pe-ca <mr.gloser1@gmail.com>
2022-05-01 13:12:27 -0500

    agregado index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander')
+const ricardo = new MissionCommander("Ricardo")
+console.log(ricardo.name)
+
+const merari = new MissionCommander("Merari")
+console.log(merari.name)
+
+const jaziel = new MissionCommander("Jaziel")
+console.log(jaziel.name)
