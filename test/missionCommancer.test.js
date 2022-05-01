71a0a0e65f6891f042c614f2982cc3ff0c9089a0
pe-ca <mr.gloser1@gmail.com>
2022-05-01 13:28:12 -0500

    agregada referencia de test a package.json y clase de test

---
+++ test/missionCommancer.test.js
@@ -0,0 +1,10 @@
+describe(
+    "Esto es una suite de pruebas", () =>{
+        test(
+            'Caso de prueba 1', ()=>{
+                const result = 1+2
+                expect(result).toBe(3)
+            }
+        )
+    }
+)
